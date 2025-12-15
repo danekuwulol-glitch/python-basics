ad12b42cf706277a0c7086d640693c396fef6074
danil <danekuwulol@gmail.com>
2025-12-15 21:57:19 +0300

    Update string_practice.py

--- string_practice.py
+++ string_practice.py
@@ -5,4 +5,10 @@ Date: 15.12.2025 (RU)
 """
 def demonstrate_strings_methods():
   """Shows all strings methods I've learned"""
-  # 1.
+  # 1. Default Methods
+text = '  Hello, World!  '
+clean = text.strip() # remove backspace ('Hello, World!)
+lower_text = text.lower() # remove caps ('hello world!')
+upper_text = text.upper() # make caps ('HELLO WORLD!')
+title_text = text.title() # capitalizes the first letter of each word
+
